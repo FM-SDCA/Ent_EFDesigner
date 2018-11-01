e48a274dbaa9f24bc15bea99bcdfd1e41bd28e36
槇森 文哉 <renlomix@hotmail.com>
2018-11-01 15:56:06 +0900

    EFデザイナ起動　DataGridViewにデータを入れたい

--- Ent_EFDesigner/Form1.Designer.cs
+++ Ent_EFDesigner/Form1.Designer.cs
@@ -29,12 +29,102 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.value = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.date = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.ex1DataSet = new Ent_EFDesigner.ex1DataSet();
+            this.ex1DataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ex1DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ex1DataSetBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id,
+            this.name,
+            this.value,
+            this.date});
+            this.dataGridView1.DataSource = this.ex1DataSetBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(35, 101);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 21;
+            this.dataGridView1.Size = new System.Drawing.Size(653, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // id
+            // 
+            this.id.HeaderText = "id";
+            this.id.Name = "id";
+            // 
+            // name
+            // 
+            this.name.HeaderText = "name";
+            this.name.Name = "name";
+            // 
+            // value
+            // 
+            this.value.HeaderText = "value";
+            this.value.Name = "value";
+            // 
+            // date
+            // 
+            this.date.HeaderText = "date";
+            this.date.Name = "date";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(35, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ex1DataSet
+            // 
+            this.ex1DataSet.DataSetName = "ex1DataSet";
+            this.ex1DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // ex1DataSetBindingSource
+            // 
+            this.ex1DataSetBindingSource.DataSource = this.ex1DataSet;
+            this.ex1DataSetBindingSource.Position = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ex1DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ex1DataSetBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn value;
+        private System.Windows.Forms.DataGridViewTextBoxColumn date;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.BindingSource ex1DataSetBindingSource;
+        private ex1DataSet ex1DataSet;
     }
 }
 
--- Ent_EFDesigner/Form1.cs
+++ Ent_EFDesigner/Form1.cs
@@ -16,5 +16,45 @@ namespace Ent_EFDesigner
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            using (var ent = new ex1Entities1())
+            {
+                int num = 0;
+                var nmodel = new extable()
+                {
+                id = new Func<int>(() =>
+                    {
+                        foreach (var en in ent.extable) num = en.id;
+                        return num + 1;
+                    })(),
+                    name = "num" + num + 1,
+                    value = 3152,
+                    time = DateTime.Now
+                };
+
+                ent.extable.Add(nmodel);
+                ent.SaveChanges();
+
+                foreach (var en in ent.extable)
+                {
+                    Console.WriteLine("id:" + en.id);
+                    Console.WriteLine("name:" + en.name);
+                    Console.WriteLine();
+                }
+            }
+
+        }
+
+        // Show
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var dt = new DataTable();
+            dataGridView1.DataSource = new ex1Entities1().extable;
+        }
+
+        // Add
     }
 }
